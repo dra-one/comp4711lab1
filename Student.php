346090f1984e7d0ee2a94d9c66699e03c2c3e9a3
dra-one <hsharma15@my.bcit.ca>
2017-09-08 12:28:21 -0700

    Completed the Student Class

--- Student.php
+++ Student.php
@@ -35,5 +35,12 @@ class Student {
         return $total / count ($this->grades);
     }
     
-    
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result.= ' (' . $this->average() . ")\n";
+        foreach ($this->emails as $which=>$what)
+            $result .= $which . ': '. $what. "\n";
+        $result .= "\n";
+        return '<pre>'.$result.'</pre>';
+    }
 }
