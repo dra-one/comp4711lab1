ee62d818056944033096803dd6a3ca7793f8d1a9
dra-one <hsharma15@my.bcit.ca>
2017-09-09 21:24:19 -0700

    Finished the index.php Commented Student.php and index.php Formatted the toString method of Student class to look better.

--- Student.php
+++ Student.php
@@ -9,10 +9,11 @@
 /**
  * Description of Student
  *
- * @author hashstyles
+ * @author Harshita Sharma, A00954584.
  */
 class Student {
     
+    // Constructor for the Student Class.
     function _construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -20,14 +21,27 @@ class Student {
         $this->grades = array();
     }
     
+    /*
+     *  Function to add an email.
+     *  $which - the type of email to be added. For ex- work, home etc.
+     *  $address - the actual email address to be added.
+     */
     function add_email($which,$address){
         $this->emails[$which] = $address;
     }
     
+    /*
+     *  Function to add grades for a student.
+     *  $grade - the grades a student got
+     */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    /*
+     * To calculate the average of the grades of the student.
+     * returning the average of a student's grade
+     */
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -35,9 +49,14 @@ class Student {
         return $total / count ($this->grades);
     }
     
+    /*
+     * Function to return the information of a student in a desired format.
+     */
     function toString() {
-        $result = $this->first_name . ' ' . $this->surname;
-        $result.= ' (' . $this->average() . ")\n";
+        $result = 'Student Name: ';
+        $result.= $this->first_name . ' ' . $this->surname;
+        $result.= "\nAverage grades: " . $this->average() . "\n";
+        $result.= "Contact Emails below:\n";
         foreach ($this->emails as $which=>$what)
             $result .= $which . ': '. $what. "\n";
         $result .= "\n";
